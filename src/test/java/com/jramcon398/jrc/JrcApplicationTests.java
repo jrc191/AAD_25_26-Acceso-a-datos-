3c653f546ab4ac7c53de93d6f7440b01591be4c1
“jrc191” <joseramoscontios@gmail.com>
2025-09-22 12:57:59 +0200

    V.Master - Add .gitignore

---
+++ src/test/java/com/jramcon398/jrc/JrcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jramcon398.jrc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JrcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
