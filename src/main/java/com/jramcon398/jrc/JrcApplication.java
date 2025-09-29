e70d85cca1b12988bd805fe05e637b961bbb3c95
“jrc191” <joseramoscontios@gmail.com>
2025-09-29 12:39:38 +0200

    V.lectura-File - Use of file library to create a file or check if file exists and where.

--- src/main/java/com/jramcon398/jrc/JrcApplication.java
+++ src/main/java/com/jramcon398/jrc/JrcApplication.java
@@ -1,13 +1,29 @@
 package com.jramcon398.jrc;
 
+import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.XSlf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+
 @SpringBootApplication
-public class JrcApplication {
+@Slf4j
+public class JrcApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JrcApplication.class, args);
 	}
 
+    @Override
+    public void run(String... args) throws Exception {
+        File file = new File("ejemplo.txt");
+        if (file.createNewFile()) {
+            log.info("Fichero creado: {}", file.getName());
+        } else {
+            log.warn("El fichero ya existe.");
+            log.info("El fichero existe en: {}", file.getAbsolutePath()); //Ruta del fichero en el disco.
+        }
+    }
 }
