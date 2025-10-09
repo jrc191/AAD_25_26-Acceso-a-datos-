2da6f35676b565750dfa65a10ec2cae2de93f679
“jrc191” <joseramoscontios@gmail.com>
2025-10-09 12:50:58 +0200

    Feature/act1_2 - V1.2 - Created binary files. We can read, write, modify... The structure is divided in: dataManipulation, dataReaderWriter, main, models, registryManager and utils. All meaningful classes are explained by comments.

--- src/main/java/com/jramcon398/jrc/main/JrcApplication.java
+++ src/main/java/com/jramcon398/jrc/main/JrcApplication.java
@@ -2,23 +2,25 @@ package com.jramcon398.jrc.main;
 
 import com.jramcon398.jrc.registryManager.RegistryManager;
 import com.jramcon398.jrc.utils.FileUtils;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.File;
 
 @SpringBootApplication
-public class JrcApplication {
+public class JrcApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
         SpringApplication.run(JrcApplication.class, args);
 
-        File file = new File("students.bin");
-        FileUtils.ensureFileExists(file);
-        RegistryManager manager = new RegistryManager();
-
-        manager.menu(file);
-
 	}
 
+    @Override
+    public void run(String... args) throws Exception {
+        File file = new File("students.dat");
+        FileUtils.ensureFileExists(file);
+        RegistryManager manager = new RegistryManager();
+        manager.menu(file);
+    }
 }
