efcedb7b78eba485b011f9a07aef88f1077103f1
“jrc191” <joseramoscontios@gmail.com>
2026-04-14 20:09:48 +0200

    Feature/act_6_1 - Refactored project to be on src folder.

--- ACT_6_1/str-consumer/src/main/java/com/jramcon398/str_consumer/repository/KafkaMessageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jramcon398.str_consumer.repository;
-
-import com.jramcon398.str_consumer.model.KafkaMessage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface KafkaMessageRepository extends JpaRepository<KafkaMessage, Long> {
-}
